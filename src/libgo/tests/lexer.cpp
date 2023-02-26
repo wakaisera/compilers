db8fa1ca37173a55d627a3a940ab5ea6d71e2dd2
Kirill Ryabov <sailor.kirill@gmail.com>
2023-02-26 18:09:29 +0700

    Add some tests

--- src/libgo/tests/lexer.cpp
+++ src/libgo/tests/lexer.cpp
@@ -1,3 +1,51 @@
-#include <libgo/lexer.hpp>
+#include <libgo/absolutepath.hpp>
+#include <libgo/dump_tokens.hpp>
 
 #include <gtest/gtest.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iterator>
+
+namespace go::test {
+
+TEST(Lexer, ValidNumbers) {
+    std::stringstream in("0 123456789");
+    std::stringstream out;
+    dump_tokens(in, out);
+    EXPECT_EQ(
+        out.str(),
+        "Loc=<1:0>\tINTEGER_LIT '0'\nLoc=<1:2>\tINTEGER_LIT '123456789'\n");
+}
+
+TEST(Lexer, IdentifiersAndComments) {
+    std::stringstream in("/* hmmm*))=) */id value // woooah\n15");
+    std::stringstream out;
+    dump_tokens(in, out);
+    EXPECT_EQ(
+        out.str(),
+        "Loc=<1:15>\tIDENTIFIER 'id'\nLoc=<1:18>\tIDENTIFIER "
+        "'value'\nLoc=<2:0>\tINTEGER_LIT '15'\n");
+}
+
+TEST(Lexer, HelloWorld) {
+    const std::filesystem::path path(c_absolute_path);
+    std::ifstream example_file(path / "examples/helloworld.go");
+    std::stringstream in(
+        {std::istreambuf_iterator<char>(example_file),
+         std::istreambuf_iterator<char>()});
+    std::stringstream out;
+    dump_tokens(in, out);
+    EXPECT_EQ(
+        out.str(),
+        "Loc=<1:0>\tPACKAGE 'package'\nLoc=<1:8>\tIDENTIFIER "
+        "'examples'\nLoc=<3:0>\tIMPORT 'import'\nLoc=<3:7>\tSTRING_LIT "
+        "'\"fmt\"'\nLoc=<6:0>\tFUNC 'func'\nLoc=<6:5>\tIDENTIFIER "
+        "'main'\nLoc=<6:9>\tL_PAREN '('\nLoc=<6:10>\tR_PAREN "
+        "')'\nLoc=<6:12>\tL_CURLY '{'\nLoc=<7:1>\tIDENTIFIER "
+        "'fmt'\nLoc=<7:4>\tDOT '.'\nLoc=<7:5>\tIDENTIFIER "
+        "'Println'\nLoc=<7:12>\tL_PAREN '('\nLoc=<7:13>\tSTRING_LIT '\"hello, "
+        "world!!\"'\nLoc=<7:29>\tR_PAREN ')'\nLoc=<8:0>\tR_CURLY '}'\n");
+}
+
+} // namespace go::test
